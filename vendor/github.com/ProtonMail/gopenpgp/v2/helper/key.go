e118038162fd0f27e45daf90d7176d632d8d1cee
Stanislav Vitko <stas@vitko.me>
2020-07-25 12:02:56 -0700

    vendor dependencies

---
+++ vendor/github.com/ProtonMail/gopenpgp/v2/helper/key.go
@@ -0,0 +1,57 @@
+package helper
+
+import (
+	"github.com/ProtonMail/gopenpgp/v2/crypto"
+)
+
+// UpdatePrivateKeyPassphrase decrypts the given armored privateKey with oldPassphrase,
+// re-encrypts it with newPassphrase, and returns the new armored key.
+func UpdatePrivateKeyPassphrase(
+	privateKey string,
+	oldPassphrase, newPassphrase []byte,
+) (string, error) {
+	key, err := crypto.NewKeyFromArmored(privateKey)
+	if err != nil {
+		return "", err
+	}
+
+	unlocked, err := key.Unlock(oldPassphrase)
+	if err != nil {
+		return "", err
+	}
+	defer unlocked.ClearPrivateParams()
+
+	locked, err := unlocked.Lock(newPassphrase)
+	if err != nil {
+		return "", err
+	}
+
+	return locked.Armor()
+}
+
+// GenerateKey generates a key of the given keyType ("rsa" or "x25519"), encrypts it, and returns an armored string.
+// If keyType is "rsa", bits is the RSA bitsize of the key.
+// If keyType is "x25519" bits is unused.
+func GenerateKey(name, email string, passphrase []byte, keyType string, bits int) (string, error) {
+	key, err := crypto.GenerateKey(name, email, keyType, bits)
+	if err != nil {
+		return "", err
+	}
+	defer key.ClearPrivateParams()
+
+	locked, err := key.Lock(passphrase)
+	if err != nil {
+		return "", err
+	}
+
+	return locked.Armor()
+}
+
+func GetSHA256Fingerprints(publicKey string) ([]string, error) {
+	key, err := crypto.NewKeyFromArmored(publicKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return key.GetSHA256Fingerprints(), nil
+}
