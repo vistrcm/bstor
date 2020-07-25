917cf162b278eacb6a05acc2e6caf0e074e83484
Stanislav Vitko <stas@vitko.me>
2020-07-25 14:19:13 -0700

    lint. no global vars in rootCommand

--- cmd/bstor/cmd/root.go
+++ cmd/bstor/cmd/root.go
@@ -7,24 +7,28 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var rootCmd = &cobra.Command{
-	Use:   "bstor",
-	Short: "bstor command to store encrypted files",
-	Long:  "a way to encrypt files and copy to external storage",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("hello")
-	},
+func rootCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "bstor",
+		Short: "bstor command to store encrypted files",
+		Long:  "a way to encrypt files and copy to external storage",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("hello")
+		},
+	}
 }
 
+//Execute main command.
 func Execute() {
-	addCommands()
+	rootCmd := rootCmd()
+	addCommands(rootCmd)
 
 	if err := rootCmd.Execute(); err != nil {
 		er(err)
 	}
 }
 
-func addCommands() {
+func addCommands(rootCmd *cobra.Command) {
 	rootCmd.AddCommand(versionCmd())
 	rootCmd.AddCommand(encryptCmd())
 	rootCmd.AddCommand(playCmd())
